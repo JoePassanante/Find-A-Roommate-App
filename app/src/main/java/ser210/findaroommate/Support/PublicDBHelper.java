f7850b6b96f424a9509aab32fa36b08597328019
Joe Passanante <j.e.passanante@gmail.com>
2018-04-15 18:20:41 -0400

    Change header

--- app/src/main/java/ser210/findaroommate/Support/PublicDBHelper.java
+++ app/src/main/java/ser210/findaroommate/Support/PublicDBHelper.java
@@ -1,6 +1,5 @@
 package ser210.findaroommate.Support;
 
-
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
